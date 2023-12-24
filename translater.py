1d624b407d813ae2928df317924c3a786393fa59
SergTa <88887008+SergTa@users.noreply.github.com>
2023-12-24 21:00:13 +0300

    Update translater.py

--- translater.py
+++ translater.py
@@ -1,7 +1,7 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
+translater = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
 
 
 translater ("I believe I can fly")
